cc0b83cfdc25ae68b122eb5371f16a83dffa7ab9
irachex <irachex@gmail.com>
2011-05-23 11:31:39 -0700

    Edited config.py via GitHub

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-COOKIE_SECRET = '366c658ff1e5001bfdaf05d8fcb33ce7'
+COOKIE_SECRET = ''
 
-DOUBAN_API_KEY = '0dcf414df4fe7994296dc3e9defb6cbc' 
-DOUBAN_API_SECRET = '02fc8d701e1f1738'
+DOUBAN_API_KEY = '' 
+DOUBAN_API_SECRET = ''
 
-WEIBO_API_KEY = '1774803516'
-WEIBO_API_SECRET = 'd414a4c912d19c038106161b42ed56b4'
+WEIBO_API_KEY = ''
+WEIBO_API_SECRET = ''
 
 TIME_DELTA = 60*10
 
 ADMIN_MAIL = 'irachex@gmail.com'
-BACKUP_MAIL = 'imdobot@gmail.com'
